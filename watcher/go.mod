1e5b9d9d3d3aba680b0e22d09042fbfd0f9d2449
Jon Johnson <jonjohnson@google.com>
2021-07-14 17:10:17 -0700

    WIP

--- watcher/go.mod
+++ watcher/go.mod
@@ -7,6 +7,7 @@ require (
 	cloud.google.com/go/spanner v1.22.0 // indirect
 	github.com/GoogleCloudPlatform/golang-samples v0.0.0-20210702130432-0beb3a0450b8 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/go-containerregistry v0.5.1 // indirect
 	github.com/gorilla/mux v1.8.0
 	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
--- watcher/main.go
+++ watcher/main.go
@@ -10,6 +10,9 @@ import (
 	"os"
 
 	"cloud.google.com/go/spanner"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/google"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/gorilla/mux"
 )
 
@@ -86,6 +89,59 @@ type Image struct {
 	Digest     string `json:"digest"`
 }
 
+type PubsubMessage struct {
+	// Attributes: Attributes for this message. If this field is empty, the
+	// message must contain non-empty data. This can be used to filter
+	// messages on the subscription.
+	Attributes map[string]string `json:"attributes,omitempty"`
+
+	// Data: The message data field. If this field is empty, the message
+	// must contain at least one attribute.
+	Data []byte `json:"data,omitempty"`
+
+	// MessageId: ID of this message, assigned by the server when the
+	// message is published. Guaranteed to be unique within the topic. This
+	// value may be read by a subscriber that receives a `PubsubMessage` via
+	// a `Pull` call or a push delivery. It must not be populated by the
+	// publisher in a `Publish` call.
+	MessageId string `json:"messageId,omitempty"`
+
+	// OrderingKey: If non-empty, identifies related messages for which
+	// publish order should be respected. If a `Subscription` has
+	// `enable_message_ordering` set to `true`, messages published with the
+	// same non-empty `ordering_key` value will be delivered to subscribers
+	// in the order in which they are received by the Pub/Sub system. All
+	// `PubsubMessage`s published in a given `PublishRequest` must specify
+	// the same `ordering_key` value.
+	OrderingKey string `json:"orderingKey,omitempty"`
+
+	// PublishTime: The time at which the message was published, populated
+	// by the server when it receives the `Publish` call. It must not be
+	// populated by the publisher in a `Publish` call.
+	PublishTime string `json:"publishTime,omitempty"`
+
+	// ForceSendFields is a list of field names (e.g. "Attributes") to
+	// unconditionally include in API requests. By default, fields with
+	// empty values are omitted from API requests. However, any non-pointer,
+	// non-interface field appearing in ForceSendFields will be sent to the
+	// server regardless of whether the field is empty or not. This may be
+	// used to include empty fields in Patch requests.
+	ForceSendFields []string `json:"-"`
+
+	// NullFields is a list of field names (e.g. "Attributes") to include in
+	// API requests with the JSON null value. By default, fields with empty
+	// values are omitted from API requests. However, any field with an
+	// empty value appearing in NullFields will be sent to the server as
+	// null. It is an error if a field in this list has a non-empty value.
+	// This may be used to include null fields in Patch requests.
+	NullFields []string `json:"-"`
+}
+
+type PushNotification struct {
+	Message      PubsubMessage `json:"message"`
+	Subscription string        `json:"subscription"`
+}
+
 // Handles API routes for mux router.
 func handleRoutes(r *mux.Router) {
 	r.HandleFunc("/ping", Ping).Methods("GET")
@@ -100,7 +156,7 @@ func Ping(w http.ResponseWriter, r *http.Request) {
 func WatchDependency(w http.ResponseWriter, r *http.Request) {
 	var wr WatchReq
 	if err := json.NewDecoder(r.Body).Decode(&wr); err != nil {
-		log.Println("Failed to decode Watch request: %v", err)
+		log.Printf("Failed to decode Watch request: %v", err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -124,7 +180,13 @@ func addDependency(ctx context.Context, wr WatchReq) error {
 
 // addImage inserts into the Spanner Images table.
 func addImage(ctx context.Context, i Image) error {
-	m := spanner.Insert("Images", imageCols, []interface{}{i.Repository, i.Tag, i.Digest})
+	m := spanner.InsertOrUpdate("Images", imageCols, []interface{}{i.Repository, i.Tag, i.Digest})
+	_, err := dbClient.Apply(ctx, []*spanner.Mutation{m})
+	return err
+}
+
+func deleteImage(ctx context.Context, i Image) error {
+	m := spanner.Delete("Images", spanner.Key{i.Repository, i.Tag})
 	_, err := dbClient.Apply(ctx, []*spanner.Mutation{m})
 	return err
 }
@@ -133,25 +195,87 @@ func addImage(ctx context.Context, i Image) error {
 // If a service (prod image) we own is updated, we ...
 // If a service dependency (base image) is updated, ... and create a Cloud Build Trigger to rebuild our service's image.
 func HandleNotification(w http.ResponseWriter, r *http.Request) {
-	var n Notification
-	if err := json.NewDecoder(r.Body).Decode(&n); err != nil {
-		log.Println("Failed to decode AR notification: %v", err)
+	log.Printf("HandleNotification: %v", r.URL)
+	var pn PushNotification
+	if err := json.NewDecoder(r.Body).Decode(&pn); err != nil {
+		log.Printf("Failed to decode Pubsub notification: %v", err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	log.Printf("%v", pn)
+
+	var n Notification
+	if err := json.Unmarshal(pn.Message.Data, &n); err != nil {
+		log.Printf("Failed to decode AR payload: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	log.Printf("%v", n)
+	i := Image{Repository: "", Tag: n.Tag, Digest: n.Digest}
+
 	// Only handle INSERT notifications.
 	if n.Action != "INSERT" {
+		if n.Action == "DELETE" {
+			if err := deleteImage(r.Context(), i); err != nil {
+				fmt.Printf("Failed to delete image %v: %v", i, err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+		}
 		w.WriteHeader(http.StatusOK)
 		return
 	}
 	// INSERT into images table.
-	i := Image{Repository: "", Tag: n.Tag, Digest: n.Digest}
 	if err := addImage(r.Context(), i); err != nil {
 		fmt.Printf("Failed to add image %v to spanner: %v", i, err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	fmt.Printf("Added image %v to spanner...\n", i)
+
+	// Check to see if this image has any dependencies.
+	ref, err := name.ParseReference(i.Digest)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	img, err := remote.Image(ref, remote.WithAuthFromKeychain(google.Keychain))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	m, err := img.Manifest()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if len(m.Annotations) != 0 {
+		if baseDigest, ok := m.Annotations["org.opencontainers.image.base.digest"]; ok {
+			if baseName, ok := m.Annotations["org.opencontainers.image.base.name"]; ok {
+				baseRef, err := name.ParseReference(baseName + "@" + baseDigest)
+				if err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+				log.Printf("%s depends on %s", ref, baseRef)
+				// TODO: Set up a pubsub subscription (if needed for cross-project stuff).
+				wr := WatchReq{
+					SourceDigest: ref.Identifier(),
+					BaseDigest:   baseDigest,
+					BaseRef:      baseName,
+				}
+
+				if err := addDependency(r.Context(), wr); err != nil {
+					fmt.Printf("Failed to add dependency %v to spanner: %v", wr, err)
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+				fmt.Printf("Added dependency %v to spanner...\n", wr)
+			}
+		}
+	}
+
+	// TODO: Just implement differ inline
 	// Call differ to get all image digests that need be rebuilt (images that depend on upstream).
 	params := fmt.Sprintf("tag=%s&digest=%s", n.Tag, n.Digest)
 	path := fmt.Sprintf("%s?%s", differURL, params)
